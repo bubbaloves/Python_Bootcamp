f4d1f036bd4b27b972aa6701a8846ab05ed3ab06
bubbaloves <105067453+bubbaloves@users.noreply.github.com>
2022-07-01 14:54:38 -0700

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,6 @@
+#print("Hello World!\nHello World!")
+print("Welcome to the Band Name Generator.")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's the name of your pet?\n")
+
+print("Congratulations! Your new band name is " + city_name + " " + pet_name)
