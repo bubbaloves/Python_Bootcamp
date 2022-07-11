33aacadad1fd0f0f1134d6ee0b17557825f36190
bubbaloves <marielena1518@gmail.com>
2022-07-11 05:32:33 -0700

    Syncing to GitHub // Frank

--- Day1.py
+++ Day1.py
@@ -4,3 +4,4 @@ city_name = input("What's the name of the city you grew up in?\n")
 pet_name = input("What's the name of your pet?\n")
 
 print("Congratulations! Your new band name is " + city_name + " " + pet_name)
+# frank is here
